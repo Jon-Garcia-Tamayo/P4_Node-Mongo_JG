2b5c7d608f74fce9808d553386801bb979417434
Jon-Garcia-Tamayo <jgarciatam@educacion.navarra.es>
2022-03-16 09:56:05 +0100

    Bootstrap linkeado

--- express.js
+++ express.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const app = express();
-
-app.use("/css", express.static("./node_modules/bootstrap/dist/css"));
-
 const path = require("path");
 
+// Set Bootstrap CSS path
+app.use("/css", express.static("./node_modules/bootstrap/dist/css"));
+
+// Set JS Bootstrap path
+app.use("/js", express.static("./node_modules/bootstrap/dist/js"));
+
+// Set the path of files for JS scripts
+app.use("/files", express.static(path.join(__dirname, "files")));
+
+// Join express.js with persona.html
 app.get("/", (request, response) => {
     response.sendFile(path.join(__dirname, "pages/library.html"));
 });
 
+// Set a listening port
 app.listen("9000", (e) => {
     console.log("[INFO] Server loaded successfully");
 })
