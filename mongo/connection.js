c7b75c9df0da1f95fe8874d9f2f887aef8ae2643
Jon-Garcia-Tamayo <jgarciatam@educacion.navarra.es>
2022-03-18 10:17:29 +0100

    Terminada conexion

--- mongo/connection.js
+++ mongo/connection.js
@@ -3,7 +3,7 @@ const connection = "mongodb://0.0.0.0/books";
 
 // Database connection
 mongoose.connect(connection, (err, res) => {
-    console.log(err ? "[ERROR] Connection failed with MongoDB" : "[INFO] Succesfully connected to MongoDB");
+    console.log(err ? "[ERROR] Connection failed with MongoDB" : "[INFO] Successfully connected to MongoDB");
 });
 
 // Creating a Schema
@@ -29,7 +29,7 @@ class BookController {
             [
                 {
                     $project: {
-                        _id: 1, title: 1, author: 1, pages: 1, genre: 1, cover: 1
+                        _id: 1, title: 1, author: 1, pages: 1, genres: 1, cover: 1
                     }
                 }
             ]
@@ -49,7 +49,7 @@ class BookController {
                 },
                 {
                     $project: {
-                        _id: 1, title: 1, author: 1, pages: 1, genre: 1, cover: 1
+                        _id: 1, title: 1, author: 1, pages: 1, genres: 1, cover: 1
                     }
                 }
             ]
@@ -57,6 +57,59 @@ class BookController {
             response.json(res);
         }).catch(err => {
             response.end(err);
+        });
+    }
+
+    // continuar aqui
+    insertBook(request, response) {
+        const id = mongoose.Types.ObjectId();
+        const title = request.body.title;
+        const author = request.body.author;
+        const pages = request.body.pages;
+        const genres = request.body.genres;
+        const cover = request.body.cover;
+
+        const objectBook = new Books(
+            {
+                "_id": id,
+                "title": title,
+                "author": author,
+                "pages": pages,
+                "genres": genres,
+                "cover": cover,
+            }
+        )
+
+        objectBook.save((err, res) => {
+            if (err) {
+                console.log("Failed to insert a new book");
+            } else {
+                console.log("ok");
+                response.json({estado: "ok"});
+            }
+        })
+    }
+
+    updateBook(request, response) {
+        const id = request.body._id;
+        const title = request.body.title;
+        const author = request.body.author;
+        const pages = request.body.pages;
+        const genres = request.body.genres;
+        const cover = request.body.cover;
+
+        Books.updateOne({title: title}, {
+            $set: {
+                title: title,
+                author: author,
+                pages: pages,
+                genres: genres,
+                cover: cover
+            }
+        }).then(res => {
+            response.json({estado: "ok"})
+        }).catch(error => {
+            response.json({estado: "error"})
         });
     }
 
